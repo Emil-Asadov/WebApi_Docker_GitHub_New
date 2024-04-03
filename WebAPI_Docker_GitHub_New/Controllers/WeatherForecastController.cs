ab54d046f25b533ac9fac16afff7892eadb95331
emil.asadov88 <emil.asadov88@gmail.com>
2024-04-03 18:39:21 +0400

    werwrwrwr

--- WebAPI_Docker_GitHub_New/Controllers/WeatherForecastController.cs
+++ WebAPI_Docker_GitHub_New/Controllers/WeatherForecastController.cs
@@ -18,8 +18,22 @@ namespace WebAPI_Docker_GitHub_New.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet]
+        [Route("GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get1()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("GetWeatherForecast1")]
+        public IEnumerable<WeatherForecast> Get2()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
